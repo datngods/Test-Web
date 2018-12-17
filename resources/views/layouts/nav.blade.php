6292dc4bb04d8813b8fb5c92e6d2d1a6c6bae9a4
DatNgo <datngo.bk@gmail.com>
2018-12-17 17:49:35 +0700

    t

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -52,7 +52,7 @@
 							</div>
 						</ul>
 					</li>
-					<li><a href="{{url('/about')}}">About Us</a></li>
+					<li><a href="/about">About Us</a></li>
 					<li><a href="{{url('/short_codes')}}">Short Codes</a></li>
 					<li><a href="{{url('/mail')}}">Mail Us</a></li>
 				</ul>
